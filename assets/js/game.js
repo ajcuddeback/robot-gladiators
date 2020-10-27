6f9116df787530887088e94bf29ef2bd76e58edd
ajcudd <ajcuddeback@gmail.com>
2020-10-27 10:25:40 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-
+//each round of enemys
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd liked to fight or run
@@ -62,9 +62,16 @@ var fight = function(enemyName) {
       }
     }
   };
-
+//begining of new round loop 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i]
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i]
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+      } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+    
 }
